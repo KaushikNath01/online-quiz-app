e9e50b753cd94a3c27dc373807ea5aea12f01717
Kaushik Nath <kaush.kn.735@gmail.com>
2021-11-01 14:57:42 +0530

    deleted adminheader.php file

--- admin.php
+++ admin.php
@@ -82,7 +82,30 @@
     </style>
 </head>
 <body>
-       <?php include './adminheader.php';?>
+       <header class="header-top" style="height:10vh;width:100%;background-color:#2978B5; color:white;">
+                 <nav style="width:80%;margin:auto;">
+                        <ul>
+                              <a href="">
+                                   <li class="dash">Dashboard</li>
+                              </a>
+                              <a href="users.php">
+                                   <li>Users</li>
+                              </a>
+                              <a href="ranking.php">
+                                   <li>Ranking</li>
+                              </a>
+                              <a href="add.php">
+                                   <li>Add Quiz</li>
+                              </a>
+                              <a href="remove.php">
+                                   <li>Edit User</li>
+                              </a>
+                              <a style="height:44px;width:120px;background-color:#FF5C58;color:white;outline:none;border:none; display:grid; place-content:center;cursor:pointer;" href="adminlogout.php">
+                                   <li>Logout</li>
+                              </a>
+                        </ul>
+                 </nav>
+       </header>
        <div class="mid">
                 <section class="content-board-menu" style="color:white;">
                                     <div class="div" style="background-color:#FF0075;">
